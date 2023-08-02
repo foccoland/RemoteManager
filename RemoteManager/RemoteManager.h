0b88187c50dfd5fa7f0c3eec79f07e64c553afee
Fabio Giannelli <fabio.giannelli@external.stellantis.com>
2023-08-02 17:57:39 +0200

    Initial Commit

---
+++ RemoteManager/RemoteManager.h
@@ -0,0 +1,18 @@
+//
+//  RemoteManager.h
+//  RemoteManager
+//
+//  Created by Fabio Giannelli on 02/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteManager.
+FOUNDATION_EXPORT double RemoteManagerVersionNumber;
+
+//! Project version string for RemoteManager.
+FOUNDATION_EXPORT const unsigned char RemoteManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteManager/PublicHeader.h>
+
+
